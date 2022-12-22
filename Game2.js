387510a4a9cdbeb4cdddd296c1c8fc43a5aeeb99
Adnan <adnanarabbaj@gmail.com>
2022-12-22 12:15:32 +0100

    Game platform en beweging
    ik heb izn platform daar gemaakt en ik heb nog gedaan dat alles mee beweegt

--- Game2.js
+++ Game2.js
@@ -4,6 +4,12 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+var speler = new Image();
+speler.src = "img/Eesrte game karakter.png"
+
+
+
+
 const gravity = 1.5
 class Player {
 constructor()  {
@@ -15,15 +21,13 @@ constructor()  {
         x:0,
         y:1
     }
-    this.whidth = 50
+    this.whidth = 50,
     this.height = 50
   }
 
   draw() {
-    c.fillStyle = "blue"
-    c.fillRect(this.position.x,
-        this.position.y, this.whidth, this.height
-    )
+
+    c.drawImage(speler, this.position.x-20, this.position.y-125, 240, 230);
   }
 
 update() {
@@ -35,10 +39,28 @@ update() {
     this.velocity.y <= canvas.height)
     this.velocity.y += gravity
     else this.velocity.y = 0
+  }
 }
+
+class Platform {
+    constructor() {
+        this.position = {
+            x: 500,
+            y: 100
+        }
+        this.width = 300
+        this.height = 20
+    }
+
+    draw() {
+        c.fillStyle = "red"
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
 }
 
 const player = new Player()
+const platform = new Platform()
+
 const keys = {
     right: {
         pressed: false
@@ -53,13 +75,29 @@ function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
+    platform.draw()
 
-    if (keys.right.pressed) {
+    if (keys.right.pressed && player.position.x <1450) {
         player.velocity.x = 5
-    }else if (keys.left.pressed) {
+    }else if (keys.left.pressed && player.position.x >165) {
         player.velocity.x = -5
+
     } else
         player.velocity.x = 0
+    if (keys.right.pressed) {
+        platform.position.x -= 5
+    } else if (keys.left.pressed) {
+        platform.position.x += 5
+    }
+    if (player.position.y + player.height
+        <= platform.position.y && player.position.y
+        + player.height + player.velocity.y >= platform.position.y
+        && player.position.x +
+        player.whidth >= platform.position.x && player.position.x <= platform.position.x
+        + platform.width
+    ) {
+        player.velocity.y = 0
+    }
 }
 
 animate()
