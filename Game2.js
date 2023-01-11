87d54e810056c6d88f375c51cec851d7ab16f285
Adnan <adnanarabbaj@gmail.com>
2023-01-11 15:07:18 +0100

    Game achtergrond en zo
    game achtergrond

--- Game2.js
+++ Game2.js
@@ -1,62 +1,72 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
-var speler = new Image();
-speler.src = "img/Eesrte game karakter.png"
+let speler = new Image();
+speler.src = "img/Eerstegamekarakter.png"
+
+
+let ag = new Image();
+ag.src = "img/Background.png";
+ag.onload = function (e) {
+    c.drawImage(ag, 10, 10, canvas.width, 1600);
+}
 
 var platform = new Image();
 platform.src = "img/Achtergrond.png"
 
 
-
 const gravity = 1.5
+
 class Player {
-constructor()  {
-    this.position = {
-        x: 100,
-        y: 100
+    constructor() {
+        this.position = {
+            x: 200,
+            y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
+        this.width = 50;
+        this.height = 50;
     }
-    this.velocity = {
-        x:0,
-        y:1
+
+    draw() {
+        c.drawImage(ag, 10, 10, canvas.width, 1600);
+        c.drawImage(speler, this.position.x - 20, this.position.y - 125, 240, 230);
     }
-    this.whidth = 50,
-    this.height = 50
-  }
 
-  draw() {
 
-    c.drawImage(speler, this.position.x-20, this.position.y-125, 240, 230);
-  }
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
 
-update() {
-    this.draw()
-    this.position.x += this.velocity.x
-    this.position.y += this.velocity.y
-
-    if (this.position.y +this.height +
-    this.velocity.y <= canvas.height)
-    this.velocity.y += gravity
-    else this.velocity.y = 0
-  }
+        if (this.position.y + this.height +
+            this.velocity.y <= canvas.height)
+            this.velocity.y += gravity
+        else this.velocity.y = 0
+    }
 }
 
 class Platform {
     constructor() {
         this.position = {
-            x: 400,
-            y: 800
+            x: 0,
+            y: 750
         }
-        this.width = 300
-        this.height = 300
+        this.height = 1800
+        this.width = 1700
     }
 
-    draw() {
 
-        c.drawImage(platform, this.position.x-20, this.position.y-125, 400, 290)
+    draw() {
+        //c.fillStyle = "red"
+        //c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        c.drawImage(platform, this.position.x - 20, this.position.y - 180, 1100, 400)
     }
 }
 
@@ -76,12 +86,12 @@ const keys = {
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
-    player.update()
     platform_two.draw()
+    player.update()
 
-    if (keys.right.pressed && player.position.x <1450) {
+    if (keys.right.pressed && player.position.x < 1450) {
         player.velocity.x = 5
-    }else if (keys.left.pressed && player.position.x >165) {
+    } else if (keys.left.pressed && player.position.x > 165) {
         player.velocity.x = -5
 
     } else
@@ -91,12 +101,10 @@ function animate() {
     } else if (keys.left.pressed) {
         platform_two.position.x += 5
     }
-    if (player.position.y + player.height
-        <= platform_two.position.y && player.position.y
-        + player.height + player.velocity.y >= platform_two.position.y
-        && player.position.x +
-        player.whidth >= platform_two.position.x && player.position.x <= platform_two.position.x
-        + platform_two.width
+    if (player.position.y + player.height <= platform_two.position.y
+        && player.position.y + player.height + player.velocity.y >= platform_two.position.y
+        && player.position.x + player.width >= platform_two.position.x
+        && player.position.x <= platform_two.position.x + platform_two.width
     ) {
         player.velocity.y = 0
     }
@@ -135,3 +143,6 @@ addEventListener('keyup', ({keyCode}) => {
     }
 
 })
+
+
+
